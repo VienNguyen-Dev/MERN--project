745925d1c83bf385bba8c162fddb6adf5badc631
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-01-29 21:41:19 +0700

    Create a middle ware and a handle funtion posible error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import User from "../models/User.model.js";
 import bcrypt from 'bcryptjs'
 
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = await bcrypt.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -10,7 +10,7 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(200).json("User created successfully")
   } catch (error) {
-    res.status(500).json(error.message)
+    next(error)
   }
 
 }
