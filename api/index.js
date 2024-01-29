e476d23dde456b9768634cfadbb9aeea32c19258
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-01-29 15:33:35 +0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("Connect to MongoDB successfuly!")
+})
+  .catch((error) => {
+    console.error(error);
+  })
 
 const app = express();
 
