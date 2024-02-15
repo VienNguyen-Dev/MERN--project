d92d81894976c8e29ec926f5da2c89bbceebeecd
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-02-15 10:23:06 +0700

    Create user update api

---
+++ api/utils/verifyUser.js
@@ -0,0 +1,14 @@
+import { errorHandle } from "./error.js";
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) return next(errorHandle(401, "Unthorizied"));
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return next(errorHandle(403, "Forbidden"));
+    req.user = user;
+    next();
+  })
+}
