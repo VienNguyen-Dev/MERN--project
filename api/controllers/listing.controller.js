77737b239d234b2b270a002b29e0a8f885b9929f
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-02-21 13:20:09 +0700

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -25,3 +25,20 @@ export const deleteListing = async (req, res, next) => {
   }
 
 }
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandle(404, "Listing not found"))
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandle(401, "You can only update your own listing"))
+  }
+  try {
+    const updateListing = await Listing.findByIdAndUpdate(req.params.id,
+      req.body, { new: true })
+    res.status(200).json(updateListing)
+  } catch (error) {
+    next(error)
+  }
+}
