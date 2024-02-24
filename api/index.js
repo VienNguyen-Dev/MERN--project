dc31e0904fb5b8b9a9f362f225f6324e354bba99
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-02-24 21:29:00 +0700

    Add pre installition

--- api/index.js
+++ api/index.js
@@ -5,7 +5,9 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import autRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
-import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser';
+import path from 'path'; //cung cấp các công cụ để làm việc với đường dẫn tệp trong ứng dụng Node.js.
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -14,6 +16,8 @@ mongoose.connect(process.env.MONGO).then(() => {
   .catch((error) => {
     console.error(error);
   })
+
+const _dirname = path.resolve(); // lấy đường dẫn tuyệt đối của thư mục hiện tại của ứng dụng và gán cho biến _dirname. Điều này hữu ích khi cần xác định đường dẫn tuyệt đối của các tệp trong ứng dụng.
 
 const app = express();
 app.use(express.json());
@@ -28,7 +32,11 @@ app.use('/api/user', userRouter)
 app.use('/api/auth', autRouter)
 app.use('/api/listing', listingRouter)
 
+app.use(express.static(path.join(_dirname, '/client/dist')));//Điều này cho phép trình duyệt truy cập các tệp CSS, JavaScript và các tài nguyên tĩnh khác của ứng dụng một cách dễ dàng.
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(_dirname, 'client', 'dist', 'index.html'))
+})
 
 
 app.use((err, req, res, next) => {
